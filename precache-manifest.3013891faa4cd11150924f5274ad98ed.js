ff7da053e4fa4abb7481c23b95e69db4e504f3da
Sahand Masoleh <sahand.masoleh@gmail.com>
2020-05-26 22:17:13 +0800

    Updates

---
+++ precache-manifest.3013891faa4cd11150924f5274ad98ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409a6d96396122d3d43d05fd023d010a",
+    "url": "/project-drum-machine/index.html"
+  },
+  {
+    "revision": "cccade44cc00e50d1230",
+    "url": "/project-drum-machine/static/css/main.f923f284.chunk.css"
+  },
+  {
+    "revision": "831a6d2045c7df47c185",
+    "url": "/project-drum-machine/static/js/2.29f4f1d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-drum-machine/static/js/2.29f4f1d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cccade44cc00e50d1230",
+    "url": "/project-drum-machine/static/js/main.4e0d3dca.chunk.js"
+  },
+  {
+    "revision": "b6667a2a500b733724a5",
+    "url": "/project-drum-machine/static/js/runtime-main.6384686f.js"
+  },
+  {
+    "revision": "f1422591b9cde9c8e2ae1f1a1ca476e1",
+    "url": "/project-drum-machine/static/media/Dited.f1422591.otf"
+  }
+]);
